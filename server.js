cfd38a3bf9778792e9d1b232e11b13ecdf352b92
EmilioNacato <93136766+EmilioNacato@users.noreply.github.com>
2024-07-23 17:13:57 -0500

    Conexion

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-process.env['TNS_ADMIN'] = '/home/ubuntu/Wallet_votoElectronicoBD';
-process.env['NODE_EXTRA_CA_CERTS'] = '/home/ubuntu/Wallet_votoElectronicoBD/ca.pem';
+process.env['TNS_ADMIN'] = '/home/ubuntu/votoElectronicoADUFA/Wallet_votoElectronicoBD';
+process.env['NODE_EXTRA_CA_CERTS'] = '/home/ubuntu/votoElectronicoADUFA/Wallet_votoElectronicoBD/ca.pem';
+
 const express = require('express');
 const path = require('path');
 const oracledb = require('oracledb');
-const fs = require('fs');
 
 const app = express();
 const port = 40000;
 
-// Ruta al directorio que contiene los archivos de wallet descargados
-const WALLET_DIR = path.join(__dirname, 'Wallet_votoElectronicoBD');
-
 // Configuración de la base de datos
 const dbConfig = {
-  user: 'ADMIN',
-  password: 'xXsCzXQjS39',
-  connectString: '(description=(retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.us-ashburn-1.oraclecloud.com))(connect_data=(service_name=gcc1a01813aadef_votoelectronicobd_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))',
-  walletLocation: WALLET_DIR,
+  user: 'ADMIN', // Usuario de la base de datos
+  password: 'xXsCzXQj@S39', // Contraseña del usuario de la base de datos
+  connectString: 'votoelectronicobd_high' // Usar el alias del tnsnames.ora
 };
 
 // Configurar la carpeta public para archivos estáticos
