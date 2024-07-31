b758d487fa2ca77497aceace3bbba79c810b0cbc
EmilioNacato <93136766+EmilioNacato@users.noreply.github.com>
2024-07-31 18:26:06 -0500

    server

--- server.js
+++ server.js
@@ -592,7 +592,7 @@ async function connectToFabric() {
     await gateway.connect(ccp, { wallet, identity: 'bmogrovejog@hotmail.com', discovery: { enabled: true, asLocalhost: false } });
 
     const network = await gateway.getNetwork('default');
-    const contract = network.getContract('roles');
+    const contract = network.getContract('rolesvoto');
 
     console.log('Successfully connected to the Hyperledger Fabric network');
     return contract;
